e86c3202933ab11bfa717f6dd14f1a043e6f07f5
francescoFH <francescofacca@gmail.com>
2021-03-12 14:02:44 +0000

    Added unit test to check that image url end with gif, jpg or png

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -16,13 +16,33 @@ class ProductTest < ActiveSupport::TestCase
                           image_url: "item.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal [ "must be greater than or equal to 0.01"], product.error[:price]
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal [ "must be greater than or equal to 0.01"], product.error[:price]
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
 
     product.price = 1
     assert product.valid?
+  end
+
+  def new_product(image_url)
+    product = Product.new(title: "My Item",
+                          description: "My Item Description",
+                          price: 1,
+                          image_url: image_url)
+  end
+
+  test "image_url" do
+    ok = %w{ pic.gif pic.jpg pic.png PIC.JPG PIC.Jpg http://a.b.c/x/y/z/pic.gif }
+    bad = %w{ pic.doc pic.gif/more pic.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
   end
 end
