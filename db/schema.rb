3c92a416221e66ef4c22a7c6fb91205b54404ed1
francescoFH <francescofacca@gmail.com>
2021-04-12 17:06:59 +0100

    Generated scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_130859) do
+ActiveRecord::Schema.define(version: 2021_04_12_160503) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
